6493ff4791f02d245d05c3fa7fed54a6a12c8a9d
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-10-03 18:52:17 -0400

    Deposit method Tested, Test Successful!

--- Driver.java
+++ Driver.java
@@ -3,7 +3,10 @@ public class Driver {
 	public static void main(String[]args) {
 		BankAccount Bank = new BankAccount(0001, 100.00 ,"SafeCase");
 		System.out.println("Account " + Bank.getAccountID() + " currently has: $" + Bank.getBalance());
-
+		Bank.setPassword("SafeHouse");
+		 if (Bank.deposit(200.00)) {
+			System.out.println("Deposit Successful!" + " Account " + Bank.getAccountID() + " now has: $" + Bank.getBalance());
+		}
 
 
 
