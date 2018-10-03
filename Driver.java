b13e524576b5652b8cc8d29c3689390b05c7a044
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-10-03 19:18:44 -0400

    Withdraw Method Tested Successfully, final changes made

--- Driver.java
+++ Driver.java
@@ -1,14 +1,26 @@
 public class Driver {
 
 	public static void main(String[]args) {
-		BankAccount Bank = new BankAccount(0001, 100.00 ,"SafeCase");
+		BankAccount Bank = new BankAccount(2381, 100.00 ,"SafeCase");
 		System.out.println("Account " + Bank.getAccountID() + " currently has: $" + Bank.getBalance());
 		Bank.setPassword("SafeHouse");
 		 if (Bank.deposit(200.00)) {
 			System.out.println("Deposit Successful!" + " Account " + Bank.getAccountID() + " now has: $" + Bank.getBalance());
 		}
-
-
+		if (Bank.withdraw(150.56)) {
+			System.out.println("Withdraw Successful!" + " Account " + Bank.getAccountID() + "now has: $" + Bank.getBalance());
+		}
+		else{
+			System.out.println("Withdraw Denied!" + " Account " + Bank.getAccountID() + " has insufficient balance." + "Account Balance: $" + Bank.getBalance());
+		}
+		if (Bank.withdraw(200.00)) {
+			 System.out.println("Withdraw Successful!" + " Account " + Bank.getAccountID() + "now has: $" + Bank.getBalance());
+		}
+		else{
+                        System.out.println("Withdraw Denied!" + " Account " + Bank.getAccountID() + " has insufficient balance." + "Account Balance: $" + Bank.getBalance());
+                }
+		System.out.println("Account Details: " + "AccountID: " + Bank.getAccountID() +  "." + " Balance: " + Bank.getBalance() +  "."); 
+		
 
 }
 }
