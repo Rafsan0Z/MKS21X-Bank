509c5c08d169beacb81193cfa37b9563deed92a0
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-10-03 14:59:33 -0400

    Constructor created!

---
+++ BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount{
+
+	private double balance;
+	private int accountID;
+	private String password;
+
+	public BankAccount(double newbalance, int newAccountID, String newPassword) {
+		balance = newbalance;
+		accountID = newAccountID;
+		password = newPassword;
+	}
+
+}
+		
+
+
